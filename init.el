87a4aaada0cc54cea19529c9e38616a179ccc2db
ko5ch3y <ko5ch3y@unseen.is>
2011-05-19 06:16:51 +0000

    Add emacs files - package.el, starter-kit and some other packages.

---
+++ init.el
@@ -0,0 +1,7 @@
+
+(load "~/.emacs.d/elpa/package.el")
+(require 'package)
+(dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
+                  ("elpa" . "http://tromey.com/elpa/")))
+  (add-to-list 'package-archives source t))
+(package-initialize)
