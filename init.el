53a122576d156c31a03295b5098ff089aa709542
ko5ch3y <ko5ch3y@unseen.is>
2011-05-23 23:03:42 +0000

    Change yellow bracket to brown in rainbow mode; remove conflicting zw mapping.

--- init.el
+++ init.el
@@ -34,7 +34,6 @@
 (vimpulse-map "zv" 'split-window-vertically)
 (vimpulse-map "zh" 'split-window-horizontally)
 (vimpulse-map "zd" 'delete-window)
-(vimpulse-map "zw" 'other-window)
 
 ;; (defun anything-find-file-other-window ()
 ;;   (interactive)
@@ -112,6 +111,20 @@
 
 (require 'rainbow-delimiters)
 (rainbow-delimiters-mode t)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "purple"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "brown"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "violet"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "cyan")))))
+
 
 (defun common-lisp-hook ()
   'rainbow-delimiters-mode
@@ -127,20 +140,6 @@
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (common-lisp-hook)))
-
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(rainbow-delimiters-depth-1-face ((t (:foreground "purple"))))
- '(rainbow-delimiters-depth-2-face ((t (:foreground "blue"))))
- '(rainbow-delimiters-depth-3-face ((t (:foreground "green"))))
- '(rainbow-delimiters-depth-4-face ((t (:foreground "yellow"))))
- '(rainbow-delimiters-depth-5-face ((t (:foreground "orange"))))
- '(rainbow-delimiters-depth-6-face ((t (:foreground "red"))))
- '(rainbow-delimiters-depth-7-face ((t (:foreground "violet"))))
- '(rainbow-delimiters-depth-8-face ((t (:foreground "cyan")))))
 
 (require 'whitespace)
 (global-whitespace-mode)
