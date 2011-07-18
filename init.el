e66cc40a184a58e64acfedec26b8327989f25dcd
ko5ch3y <ko5ch3y@unseen.is>
2011-07-18 14:44:48 +1000

    Rename Lolokai theme to Nature.

--- init.el
+++ init.el
@@ -12,8 +12,8 @@
   (global-hl-line-mode 0)
   (add-to-list 'load-path "~/.emacs.d/site-lisp/color-theme-6.6.0")
   (require 'color-theme)
-  (require 'color-theme-lolokai)
-  (color-theme-lolokai))
+  (require 'color-theme-nature)
+  (color-theme-nature))
 
 
 (require 'undo-tree)
