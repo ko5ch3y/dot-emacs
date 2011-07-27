e4e9ac2c5457bd04dd4235e4a76a9393c6ff401a
ko5ch3y <ko5ch3y@unseen.is>
2011-07-27 11:24:54 +1000

    Better (maybe) emacs compile script.

--- compile-all-files.sh
+++ compile-all-files.sh
@@ -2,7 +2,16 @@
 
 FTC=files-to-compile.txt
 
+# adapted from org-mode Makefile
+EMACS=emacs
+prefix=/usr
+lispdir=$(prefix)/share/emacs/site-lisp
+
+BATCH=$(EMACS) -batch -q -no-site-file -eval \
+  "(setq load-path (cons (expand-file-name \"./lisp/\") (cons \"$(lispdir)\" load-path)))"
+
 find -type f -name '*.el' > $FTC
-emacs -batch -f batch-byte-compile `cat $FTC`
+# emacs -batch -f batch-byte-compile `cat $FTC`
+$(BATH) `cat $FTC`
 rm -f $FTC
 
