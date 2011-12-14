5516316be84b3b026ab81d91c78bdb6b3b762770
ko5ch3y <ko5ch3y@unseen.is>
2011-12-14 14:37:15 +1100

    Better batch compile script.

--- compile-all-files.sh
+++ compile-all-files.sh
@@ -3,6 +3,6 @@
 FTC=files-to-compile.txt
 
 find -type f -name '*.el' > $FTC
-emacs -batch -f batch-byte-compile `cat $FTC`
+emacs --batch -Q -L . -L lib -f batch-byte-compile `cat $FTC`
 rm -f $FTC
 
