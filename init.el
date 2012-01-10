ae6bcd7ca442103737033cf399b9c43283f267f5
ko5ch3y <ko5ch3y@unseen.is>
2012-01-10 15:24:51 +1100

    Set compilation-mode initial evil state to normal.

--- init.el
+++ init.el
@@ -310,7 +310,8 @@
   (evil-mode 1)
 
   (evil-set-initial-state 'completion-list-mode 'emacs)
-  (evil-set-initial-state 'occur-mode 'normal))
+  (evil-set-initial-state 'occur-mode 'normal)
+  (evil-set-initial-state 'compilation-mode 'normal))
 
 (defun my-scheme-complete-setup ()
   (autoload 'scheme-smart-complete "scheme-complete" nil t)
