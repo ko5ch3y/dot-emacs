d5b9275db4759a2c39301138d0f0833da5f243c9
ko5ch3y <ko5ch3y@unseen.is>
2012-01-13 13:56:07 +1100

    Show entire expression inside matching parens.

--- init.el
+++ init.el
@@ -417,7 +417,7 @@ This arrangement depends on the value of `gdb-many-windows'."
   (setq-default visible-bell t)
   (fset 'yes-or-no-p 'y-or-n-p)
   (setq-default show-paren-delay 0)
-  (setq-default show-paren-style 'mixed)
+  (setq-default show-paren-style 'expression)
   (setq-default grep-command "grep --exclude-from=$HOME/.grepignore -niHIR -e ")
   (setq initial-scratch-message "")
   (add-hook 'before-save-hook 'delete-trailing-whitespace)
