37dc5bb99fd98124360a2e02e3dc6bd2a778e360
ko5ch3y <ko5ch3y@unseen.is>
2012-01-14 14:19:44 +1100

    Set org-mode standard-indent 2.

--- init.el
+++ init.el
@@ -217,6 +217,7 @@
   (require 'org-install)
   (require 'org-mw)
   (setq-default initial-major-mode 'org-mode)
+  (add-hook 'find-file-hook '(lambda () (setq standard-indent 2)))
   (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
   (add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode)))
 
