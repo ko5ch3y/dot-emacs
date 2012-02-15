e6110eb6a7d862925afb5a8a5ce3b8ce6be3d550
ko5ch3y <ko5ch3y@unseen.is>
2012-02-15 16:46:52 +1100

    Add M-n/p etc. maps to anything-generic-files-map to work on recent files list etc.

--- init.el
+++ init.el
@@ -498,6 +498,12 @@ This arrangement depends on the value of `gdb-many-windows'."
   (define-key anything-c-buffer-map "\M-s" 'anything-buffer-save-persistent)
   (define-key anything-c-buffer-map "\M-w" 'paredit-backward-kill-word)
 
+  (define-key anything-generic-files-map "\M-n" 'anything-next-line)
+  (define-key anything-generic-files-map "\C-n" 'next-history-element)
+  (define-key anything-generic-files-map "\M-o" 'anything-buffer-or-ff-run-switch-other-window)
+  (define-key anything-generic-files-map "\M-p" 'anything-previous-line)
+  (define-key anything-generic-files-map "\C-p" 'previous-history-element)
+
   (define-key anything-find-files-map "\M-e" 'anything-execute-persistent-action)
   (define-key anything-find-files-map "\M-h" 'paredit-backward-delete)
   (define-key anything-find-files-map "\M-H" 'paredit-backward-delete)
