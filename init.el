18dbfc039e86dc7e034740c59ed2d060b45ebced
ko5ch3y <ko5ch3y@unseen.is>
2013-08-22 16:37:52 +1000

    Update loading of org-mode

--- init.el
+++ init.el
@@ -222,8 +222,7 @@
   (load "elscreen" "ElScreen" t))
 
 (defun my-org-mode-setup ()
-  (require 'org-install)
-  (require 'org-mw)
+  (require 'org)
   (setq-default initial-major-mode 'org-mode)
   (add-hook 'find-file-hook '(lambda () (setq standard-indent 2)))
   (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
