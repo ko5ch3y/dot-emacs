1e75bf5c5d802acc252c51663ce70f4770977de2
ko5ch3y <ko5ch3y@unseen.is>
2013-12-16 15:30:51 +1100

    el-get: Add startup code

--- init.el
+++ init.el
@@ -1085,3 +1085,37 @@ the line, to capture multiline input. (This only has effect if
 (my-tab-map-setup)
 (my-term-map-setup)
 (my-org-mode-evil-map-setup)
+
+
+(require 'cl)
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(defun get-el-get ()
+  (unless (require 'el-get nil t)
+    (url-retrieve "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
+                  (lambda (s)
+                    (end-of-buffer)
+                    (eval-print-last-sexp)))))
+
+(get-el-get)
+
+
+;; set local recipes
+(setq el-get-sources
+      '(
+        ))
+
+;; set own packages
+(setq my:el-get-packages
+      '(el-get
+        ))
+
+(setq my:el-get-packages
+      (append my:el-get-packages
+              (loop for src in el-get-sources collect (el-get-source-name src))))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
+;; install new packages and init already installed packages
+(el-get 'sync my:el-get-packages)
